4ff64bcb61f151be411716e541eca2c05d3b4e32
Martin Enchev <m_enchevbg@yahoo.com>
2022-05-20 11:51:10 +0300

    A solution, using dictionary.
    It has some mistake, and works only in 33 % of the test cases.

---
+++ adAstra.js
@@ -0,0 +1,57 @@
+function adAstra(inputArray) {
+
+    /*
+    Здравейте. Изпитна зад. Ad Astra, от Fundamentals JS, като я правя с речник дава само 33 % Преправям я, да е с масив, и веднага получавам 100 % .. (zero тестовете не гърмят на речник-варианта..) Това е вариантът ми с речник: https://pastebin.com/tGh09EEV (усл: judge>Fundam-Exams>01.Final)
+    */
+
+    // ?? tova e s rechnik i dava 33 % ; prepravih go s masiv i vednaga dade 100 % ...  
+
+    let pattern = /([#]|[\|])(?<itemName>[a-zA-Z\s]+)\1(?<expirDate>[0-9]{2}[\/][0-9]{2}[\/][0-9]{2})\1(?<calories>[0-9]{1,5})\1/g;
+    let text = inputArray[0];
+    const CALOR_PER_DAY = 2000;
+
+    let foodItems = {};
+    let calorInFood = 0;
+
+    let match = pattern.exec(text);
+
+    while (match != null) {
+
+        let itemName = match.groups["itemName"];
+        let expirDate = match.groups["expirDate"];
+        let calories = Number(match.groups["calories"]);
+        calorInFood += calories
+
+        foodItems[itemName] = {
+            itemName,
+            expirDate,
+            calories,
+        };
+
+        match = pattern.exec(text);
+    };
+
+    let days = Math.trunc(calorInFood / CALOR_PER_DAY);
+    console.log(`You have food to last you for: ${days} days!`);
+
+    for (let item in foodItems) {
+        let name = foodItems[item].itemName;
+        let expirDate = foodItems[item].expirDate;
+        let calories = foodItems[item].calories;
+
+        console.log(`Item: ${name}, Best before: ${expirDate}, Nutrition: ${calories}`);
+    };
+};
+
+adAstra([
+    '#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|'
+]);
+
+console.log(`- - - - - - - -`);
+
+adAstra(['Hello|#Invalid food#19/03/20#450|$5*(@']);
+
+console.log(`- - - - - - - -`);
+
+adAstra(['$$#@@%^&#Fish#24/12/20#8500#|#Incorrect#19.03.20#450|$5*(@!#Ice Cream#03/10/21#9000#^#@aswe|Milk|05/09/20|2000|']);
+
